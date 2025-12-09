a40e86c9155935701ab111858b7f2249b1d7818d
nathanDuncan <20ntd1@queensu.ca>
2025-12-08 19:38:21 -0500

    Fix base path for production

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 export default defineConfig({
-  base: "/nathanDuncan.github.io/",
+  base: "/",
 
   plugins: [
     react(),
